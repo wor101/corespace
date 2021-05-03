4fe13302566a9dc1a658fd782f1c605f6a0abe2d
Will <wor101@gmail.com>
2021-05-03 19:15:23 +0000

    added character classes yaml and pages

--- corespace.rb
+++ corespace.rb
@@ -22,11 +22,31 @@ helpers do
   def load_yaml(file_path)
     data_file = File.open(file_path)
     yaml_file = YAML.safe_load(data_file)
+    data_file.close
+    yaml_file
   end
 
   def load_skills
     load_yaml(data_path + '/skills.yaml')
   end
+    
+  def load_classes
+    load_yaml(data_path + '/classes.yaml')
+  end
+  
+  def load_class_skills(char_class)
+    skills = load_skills
+    all_skills_hash = {}
+
+    skills.each_pair do |category, skills|
+      skills.each_pair do |skill, details|
+        all_skills_hash[skill] = details
+      end
+    end
+
+    all_skills_hash.select { |skill, details|  @char_class['skills'].keys.include?(skill) } 
+  end
+
 end
 
 get '/' do
@@ -53,4 +73,18 @@ get '/skills/:category/:skill' do
   skill_name = params[:skill]
   @skill = load_skills[category_name][skill_name]
   erb :skill, layout: :layout
-end 
+end
+
+get '/classes' do
+  @classes = load_yaml(data_path + '/classes.yaml')
+  
+  erb :classes, layout: :layout
+end
+
+get '/classes/:class_name' do 
+  @classes = load_classes
+  @char_class = @classes[params[:class_name]]
+  @class_skills = load_class_skills(@char_class)
+  
+  erb :class, layout: :layout
+end
